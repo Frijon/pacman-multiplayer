fa3463499632d77ce429ec0838ac684d0bef4d56
Orios <barinthe1st@gmail.com>
2020-10-28 19:48:07 +0100

    added background

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,53 @@
 package com.company;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Main {
 
-    public static void main(String[] args) {
 
-        Window gamewindow = new Window();
+    public static void main(String[] args) {
+        // Erzeugung eines neuen Dialoges
+        JFrame Frame = new JFrame();
+        Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Frame.setTitle("JPanel Beispiel");
+        Frame.setSize(800,600);
+        Image background = Toolkit.getDefaultToolkit().createImage("com/company/index.png");
+
+        Window panel = new Window();
+
+
+        /*JButton button1 = new JButton("Click!");
+        JButton button2 = new JButton("Me");
+        ActionListener l = new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(e.getSource() == button1){
+                    System.out.println("CLICK!");
+                }
+                if(e.getSource() == button2){
+                    System.out.println("Me!");
+                }
+            }
+        };
+
+        button1.addActionListener(l);
+        button2.addActionListener(l);
+
+        // Hier fügen wir unserem Dialog unser JPanel hinzu
+
+
+        Frame.add(button1,BorderLayout.SOUTH);
+        Frame.add(button2,BorderLayout.NORTH);  */
+
+        Frame.add(panel,BorderLayout.CENTER);
+        Frame.setVisible(true);
 
 
     }
--- src/com/company/Window.java
+++ src/com/company/Window.java
@@ -1,19 +1,31 @@
 package com.company;
 
-import javax.swing.*;
 import java.awt.*;
-//class BackgroundImageJFrame extends JFrame
+import javax.swing.*;
 
-public class Window {
+public class Window extends JPanel {
 
-    public  Window(){
-        JFrame window = new JFrame();
-        window.setSize(1920, 1080);
-        //window.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        window.getContentPane().setBackground(Color.cyan);
-        window.setTitle("PacMan");
-        window.setVisible(true);
-        //window.setContentPane(new JLabel(new ImageIcon("C:\\Users\\barin\\Downloads")));
-        //test
+    Image backgroundimg = Toolkit.getDefaultToolkit().createImage("D://github/Pacman multiplayer/src/com/company/index.png");
+    Image scaledImage = backgroundimg.getScaledInstance(800, 600, 0);
+
+    public Window() {
+        setBackground(Color.cyan);
+       /* ImageIcon icon = new ImageIcon("com/company/index.png");
+        JLabel thumb = new JLabel();
+        thumb.setIcon(icon);*/
     }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(scaledImage, 0, 0, this);
+        System.out.println("test");
+    }
+
+   /* @Override
+    public void paint(Graphics g) {
+        super.paintComponents(g);
+        g.drawImage(backgroundimg, 0, 0, null);
+    }*/
+
 }
