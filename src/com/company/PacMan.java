c5e2a9ccf264efb874f6b1b0215f75d131923243
elias.goerlitz <barinthe1st@gmail.com>
2020-11-18 15:29:51 +0100

    changed backgroundimg, game over screen set everything to 1080p and fullscreen

--- src/com/company/PacMan.java
+++ src/com/company/PacMan.java
@@ -15,12 +15,7 @@ public class PacMan {
 
     void Movement (){
 
-       /* if (Window.Keys[0]) y -= 10;
-        if (Window.Keys[1]) x -= 10;
-        if (Window.Keys[2]) x += 10;
-        if (Window.Keys[3]) y += 10;*/
-
-        if ((this.x >= (0 - size) && this.x <= Main.GAME_WIDTH) && (this.y >= (0 - size) && this.y <= Main.GAME_HEIGHT)) {
+        if ((this.x >= (- size) && this.x <= Main.GAME_WIDTH) && (this.y >= (- size) && this.y <= Main.GAME_HEIGHT)) {
             switch (this.dir) {
                 case "up":
                     y -= 10;
@@ -37,18 +32,22 @@ public class PacMan {
             }
         }
 
-//        System.out.println("this.x = " + this.x);
-//        System.out.println("this.y = " + this.y);
 
-        if (this.x < 0 - size) this.x = Main.GAME_WIDTH;
-        if (this.x > Main.GAME_WIDTH) this.x = 0 - size;
-        if (this.y < 0 - size) this.y = Main.GAME_HEIGHT;
-        if (this.y > Main.GAME_HEIGHT) this.y = 0 - size;
+        if (this.x < -size) this.x = Main.GAME_WIDTH;
+        if (this.x > Main.GAME_WIDTH) this.x = - size;
+        if (this.y < - size) this.y = Main.GAME_HEIGHT;
+        if (this.y > Main.GAME_HEIGHT) this.y = - size;
 
         for (int i = 0; i < Window.Ghosts.length; i++){
-            if ((((this.x + size) > Window.Ghosts[i].posx) && (this.x < (Window.Ghosts[i].posx + Window.Ghosts[i].size))) && ((this.y < (Window.Ghosts[i].posy + Window.Ghosts[i].posy)) && ((this.y + this.size) > Window.Ghosts[i].posy))){
+            if ((((this.x + size) > Window.Ghosts[i].posx) && (this.x < (Window.Ghosts[i].posx + Window.Ghosts[i].size))) && ((this.y < (Window.Ghosts[i].posy + Window.Ghosts[i].size)) && ((this.y + this.size) > Window.Ghosts[i].posy))){
                 System.out.println("Game lost!");
+                Main.run = false;
             }
+
+            /*Color c = new Color(Window.scaledImage.getRGB(this.x, this.y));
+            if (c.getRed() < 30 && c.getGreen() > 225 && c.getBlue() > 225) {
+
+            }*/
         }
 
     }
